1dc6bdb6c1c9cf2b8b8aaa0daf46d93c57bf9b1d
Shuyang Li <shuyang.li.95@gmail.com>
2015-02-11 10:54:02 -0500

    make sure we're evaluating the correct line, and the level number is correct

--- rscheme.rb
+++ rscheme.rb
@@ -27,19 +27,29 @@ loop do
     line.strip!
 
     all_lines << line
-    tokens = lexer.lex(line)
+    tokens = lexer.lex(all_lines)
     syntax_tree = parser.parse(tokens)
     eval_result = evaluator.evaluate(syntax_tree)
     puts eval_result.inspect
+
+    all_lines = ""
+    level = 0
+
   rescue RScheme::RSchemeExprNotTerminatedError => ex
     all_lines << "\n"
     level = ex.message
   rescue RScheme::RSchemeLexingError => ex
     warn "[LEXING ERROR]: #{ex.message}"
+    all_lines = ""
+    level = 0
   rescue RScheme::RSchemeParsingError => ex
     warn "[PARSING ERROR]: #{ex.message}"
+    all_lines = ""
+    level = 0
   rescue RScheme::RSchemeRuntimeError => ex
     warn "[RUNTIME ERROR]: #{ex.message}"
+    all_lines = ""
+    level = 0
   end
 
 end
